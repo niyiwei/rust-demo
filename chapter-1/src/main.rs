b3401699f61c6e5586a13fd8297fb27f0f1500f6
niyiwei <panhaidon@qq.com>
2022-01-07 23:31:08 +0800

    feat: rustwiki - chapter-1: learning print

---
+++ chapter-1/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    println!("Learning print");
+
+    println!("Expect: abc!{}", "abc");
+
+    println!("Expect: 1 2 1!{0} {1} {0}", "1", "2");
+
+    println!("Expect: aaa!{abc}", abc = "aaa");
+
+    println!("Expect     5!{number:0width$}", number = 5, width = 5);
+    println!("Expect 00005!{number:>0width$}", number = 5, width = 5);
+
+    #[derive(Debug)]
+    struct Abc(i32);
+
+    println!("Print struct:{:?}", Abc(1));
+    println!("Print struct:{:#?}", Abc(2));
+
+    println!("{:?}", 100);
+
+    println!("{1:?} {0:?} is the {actor:?} name.", "Slater", "Christian", actor = "actor's");
+
+    #[derive(Debug)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8,
+    }
+
+    let person = Person { name: "abc", age: 85 };
+    println!("person:{:?}", person);
+    println!("person:{:#?}", person);
+}
+
+mod tests {}
